3f17f65e88a9bb30988bf0c0375d638c6372eb55
krzysztof <Kryzysk09@gmail.com>
2019-01-05 21:45:05 +0100

    Uniwersalizacja rysowania pionków poszczególnych graczy (jest w ogóle takie słowo? Nie wiem, ale brzmi mądrze)

--- src/main/java/Client/Board/FillBoard.java
+++ src/main/java/Client/Board/FillBoard.java
@@ -9,10 +9,11 @@ import javafx.stage.Stage;
 public class FillBoard {
     public GridPane grid;
     public Color color;
-    public FillBoard(int numberOfPlayers, Stage primaryStage, Color color) {
-        BoardDraw bd = new BoardDraw(primaryStage, ServerMain.board);
+    public FillBoard(int numberOfPlayers, Stage primaryStage, Color color, StarBoard board) {
+        BoardDraw bd = new BoardDraw(primaryStage, board);
         this.grid=bd.getGrid();
         this.color=color;
-        PiecesDraw piecesDraw = new PiecesDraw(numberOfPlayers, bd.getGrid(), color);
+        PiecesDraw piecesDraw = new PiecesDraw(numberOfPlayers, bd.getGrid(), color, board);
+        piecesDraw.drawPieces();
     }
 }
